1a41f2b6b8f2ccce7733189af24dd28fdcc40fce
Cleverson Nahum <cleversonahum@gmail.com>
2024-12-10 15:58:53 -0300

    Using local python environment without VM

--- Vagrantfile
+++ Vagrantfile
@@ -8,17 +8,17 @@ Vagrant.configure("2") do |config|
   config.vm.box = "generic/ubuntu2204"
   config.vm.hostname = "mininet"
   config.vm.disk :disk, size: "20GB", primary: true
-  config.vm.synced_folder './', "/home/vagrant/mgen_mininet"
-  config.vm.network 'private_network', ip: '192.168.50.10'
+  #config.vm.synced_folder './', "/home/vagrant/mgen_mininet"
+  #config.vm.network 'private_network', ip: '192.168.50.10'
 
-  config.vm.provision "init", type: "shell", inline: <<-SHELL
-    sudo apt update -y && sudo apt install -y software-properties-common mininet=2.2.2-5ubuntu1 python3-pip screen libpcap-dev
-    sudo add-apt-repository ppa:deadsnakes/ppa -y && sudo apt update -y && sudo apt install python3.12 -y
-    curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.12
-    sudo pip3.12 install --upgrade setuptools six requests
-    sudo pip3.12 install mininet networkx schedule
-    git clone https://github.com/USNavalResearchLaboratory/mgen.git && cd mgen && git submodule update --init && git checkout V5.1.1 && cd makefiles && make -f Makefile.linux
-  SHELL
+  #config.vm.provision "init", type: "shell", inline: <<-SHELL
+  #  sudo apt update -y && sudo apt install -y software-properties-common mininet=2.2.2-5ubuntu1 python3-pip screen libpcap-dev
+  #  sudo add-apt-repository ppa:deadsnakes/ppa -y && sudo apt update -y && sudo apt install python3.12 -y
+  #  curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.12
+  #  sudo pip3.12 install --upgrade setuptools six requests
+  #  sudo pip3.12 install mininet networkx schedule
+  #  git clone https://github.com/USNavalResearchLaboratory/mgen.git && cd mgen && git submodule update --init && git checkout V5.1.1 && cd makefiles && make -f Makefile.linux
+  #SHELL
   # config.vm.provision "start", type: "shell", run: "always", inline: <<-SHELL
   #   sudo mn -c && sudo killall -q -w python3.12
   #   set -o allexport; source /home/vagrant/demo-erday/system.env; set +o allexport
--- network_scenario.py
+++ network_scenario.py
@@ -1,64 +1,93 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-"""
-Simple example of setting network and CPU parameters
-
-NOTE: link params limit BW, add latency, and loss.
-There is a high chance that pings WILL fail and that
-iperf will hang indefinitely if the TCP handshake fails
-to complete.
-"""
-
-from sys import argv
-
-from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import CPULimitedHost
-from mininet.link import TCLink
-from mininet.util import dumpNodeConnections
+from mininet.node import RemoteController
+from mininet.node import Host
+from mininet.node import OVSKernelSwitch
+from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+from mininet.link import TCLink
+import networkx as nx
+from typing import Optional
 
 
-# It would be nice if we didn't have to do this:
-# pylint: disable=arguments-differ
+class Network:
+    def __init__(self, topo_file: str, topo_params: Optional[dict] = None):
+        self.net_topology = nx.read_gml(topo_file)
+        self.net = Mininet(
+            controller=RemoteController, switch=OVSKernelSwitch, link=TCLink
+        )
+        self.net.addController("c0")
+        self.switches = []
+        self.hosts = []
+        if topo_params is None:
+            topo_params = {
+                "bandwidth": "bandwidth",
+                "delay": "delay",
+                "loss": "loss",
+            }
+        link_default_values = {
+            "bandwidth": 100,
+            "delay": "1ms",
+            "loss": 0,
+        }
+
+        # Creating switches and hosts
+        for node in self.net_topology.nodes:
+            id = int(node) + 1  # Avoid id 0
+            # Switch
+            curr_switch = self.net.addSwitch(
+                f"s{id}",
+                cls=OVSKernelSwitch,
+                protocols="OpenFlow10",
+                mac_address=self.gen_mac_address(id),
+            )
+            self.switches.append(curr_switch)
+            # Host
+            curr_host = self.net.addHost(
+                f"h{id}",
+                cls=Host,
+            )
+            self.hosts.append(curr_host)
+            # Creating links between switch and host
+            self.net.addLink(self.hosts[int(id) - 1], self.switches[int(id) - 1])
+
+        # Creating switch links
+        for edge in self.net_topology.edges:
+            src = int(self.net_topology.edges[edge[0]]["id"]) + 1
+            dst = int(self.net_topology.edges[edge[1]]["id"]) + 1
+            bw = self.net_topology.edges[edge].get(
+                topo_params["bandwidth"], link_default_values["bandwidth"]
+            )
+            delay = self.net_topology.edges[edge].get(
+                topo_params["delay"], link_default_values["delay"]
+            )
+            loss = self.net_topology.edges[edge].get(
+                topo_params["loss"], link_default_values["loss"]
+            )
+            self.net.addLink(
+                self.switches[src - 1],
+                self.switches[dst - 1],
+                bw=bw,
+                delay=delay,
+                loss=loss,
+            )
+
+    def start(self):
+        self.net.start()
+        info("Starting network\n")
+        CLI(self.net)
+        self.net.stop()
+
+    def gen_mac_address(self, id):
+        return f"00:00:00:00:00:{id:02x}"
 
 
-class SingleSwitchTopo(Topo):
-    "Single switch connected to n hosts."
-
-    def build(self, n=2, lossy=True):
-        switch = self.addSwitch("s1")
-        for h in range(n):
-            # Each host gets 50%/n of system CPU
-            host = self.addHost("h%s" % (h + 1), cpu=0.5 / n)
-            if lossy:
-                # 10 Mbps, 5ms delay, 10% packet loss
-                self.addLink(host, switch, bw=10, delay="5ms", loss=10, use_htb=True)
-            else:
-                # 10 Mbps, 5ms delay, no packet loss
-                self.addLink(host, switch, bw=10, delay="5ms", loss=0, use_htb=True)
-
-
-def perfTest(lossy=True):
-    "Create network and run simple performance test"
-    topo = SingleSwitchTopo(n=4, lossy=lossy)
-    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, autoStaticArp=True)
-    net.start()
-    info("Dumping host connections\n")
-    dumpNodeConnections(net.hosts)
-    info("Testing bandwidth between h1 and h4 (lossy=%s)\n" % lossy)
-    h1, h4 = net.getNodeByName("h1", "h4")
-    net.iperf((h1, h4), l4Type="UDP")
-    # Debugging
-    h1.cmd("jobs")
-    h4.cmd("jobs")
-    net.stop()
+def main():
+    setLogLevel("info")
+    network = Network("topology.txt")
+    network.start()
 
 
 if __name__ == "__main__":
-    setLogLevel("info")
-    # Debug for now
-    if "testmode" in argv:
-        setLogLevel("debug")
-    # Prevent test_simpleperf from failing due to packet loss
-    perfTest(lossy=("testmode" not in argv))
+    main()
