28b1808f1b1a5bd71e0b93fbcff13eb6f276b686
Cleverson Nahum <cleversonahum@gmail.com>
2024-12-11 17:07:35 -0300

    Generating the network topology and starting to implement mgen servers

--- network_scenario.py
+++ network_scenario.py
@@ -15,7 +15,7 @@ class Network:
     def __init__(self, topo_file: str, topo_params: Optional[dict] = None):
         self.net_topology = nx.read_gml(topo_file)
         self.net = Mininet(
-            controller=RemoteController, switch=OVSKernelSwitch, link=TCLink
+            # controller=RemoteController, switch=OVSKernelSwitch, link=TCLink
         )
         self.net.addController("c0")
         self.switches = []
@@ -56,9 +56,9 @@ class Network:
         for edge in self.net_topology.edges(data=True):
             src = int(edge[0])
             dst = int(edge[1])
-            bw = edge.get(topo_params["bandwidth"], link_default_values["bandwidth"])
-            delay = edge.get(topo_params["delay"], link_default_values["delay"])
-            loss = edge.get(topo_params["loss"], link_default_values["loss"])
+            bw = edge[2].get(topo_params["bandwidth"], link_default_values["bandwidth"])
+            delay = edge[2].get(topo_params["delay"], link_default_values["delay"])
+            loss = edge[2].get(topo_params["loss"], link_default_values["loss"])
             self.net.addLink(
                 self.switches[src],
                 self.switches[dst],
@@ -70,18 +70,63 @@ class Network:
     def start(self):
         self.net.start()
         info("Starting network\n")
+        # self.hosts[0].cmd("mgen input client.mgn &")
+        # self.hosts[1].cmd("mgen input server.mgn &")
         CLI(self.net)
         self.net.stop()
 
     def gen_mac_address(self, id):
         return f"00:00:00:00:00:{id:02x}"
 
+    def start_servers(self, flows_description):
+        for flow, info in flows_description.items():
+            src = self.hosts[info["src"]]
+            dst = self.hosts[info["dst"]]
+            protocol = info["protocol"]
+            port = info["port"]
+            duration = info["duration"]
+            traffic_pattern = info["traffic_pattern"]
+            windows = info["report_period"]
+
+            dst.cmd(
+                f"h{dst} mgen port {port} report analytics window {windows} output logs/host_{dst}.log"
+            )
+
 
 def main():
     setLogLevel("info")
-    network = Network("topology.txt")
+    topo_params = {
+        "bandwidth": "capacity",
+        "delay": "delay",
+        "loss": "loss",
+    }
+    flows_description = {
+        "flow1": {
+            "src": 0,
+            "dst": 2,
+            "protocol": "TCP",
+            "port": 5001,
+            "duration": 10,
+            "traffic_pattern": "periodic",  # Can be periodic periodic, poisson, burst
+            "report_period": 1,  # Seconds
+        },
+        "flow2": {
+            "src": 0,
+            "dst": 2,
+            "protocol": "UDP",
+            "port": 5002,
+            "duration": 10,
+            "traffic_pattern": "poisson",  # Can be periodic periodic, poisson, burst
+            "report_period": 1,  # Seconds
+        },
+    }
+    network = Network("simple.txt", topo_params)
     network.start()
 
 
 if __name__ == "__main__":
     main()
+
+# h2 mgen port 5001,5002 report analytics window 1 output logs/server.log
+# h2 {mgen port 5001,5002 report analytics window 1 2>&1 | grep 'REPORT' > logs/server.log;} &
+# h0 mgen report analytics window 1 event "0.0 ON 1 UDP SRC 5001 DST 10.0.0.3/5001 PERIODIC [1000 1024]" event "10.0 OFF 1"
